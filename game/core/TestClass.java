6c02b3ffef80cfba9435c072f84a7949ce8e56a4
David Kambersky <david.kambersky@gmail.com>
2012-09-10 18:57:55 +0200

    FINAL COMMIT OF THIS DAY FK IT
    yeah.

---
+++ game/core/TestClass.java
@@ -0,0 +1,18 @@
+// Class created by xCabbage [github.com/xcabbage] 
+// for the trololus project [github.com/xcabbage/trololus]
+
+
+
+/**
+ * 
+ */
+package game.core;
+
+/**
+ * @author David
+ *
+ */
+public class TestClass {
+
+}
+
