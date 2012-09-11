487baa4d603c87bfaba20fff0807fea05850d7f5
David Kamberský <david.kambersky@gmail.com>
2012-09-11 13:35:43 +0200

    added GameField class

--- src/game/core/GameField.java
+++ src/game/core/GameField.java
@@ -1,5 +1,13 @@
+// Class created by xCabbage [github.com/xcabbage] 
+// for the trololus project [github.com/xcabbage/trololus]
+
 package game.core;
+
+import org.newdawn.slick.*;
 
 public class GameField {
 
+	public GameField() {
+		
+	}
 }
