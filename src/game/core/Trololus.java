0166af5145319c89a75d356b013e04d21a020cc3
David Kambersky <david.kambersky@gmail.com>
2012-09-11 16:12:21 +0200

    Added new strings and display switches and shit

--- src/game/core/Trololus.java
+++ src/game/core/Trololus.java
@@ -1,7 +1,6 @@
 // Class created by xCabbage [github.com/xcabbage] 
 // for the trololus project [github.com/xcabbage/trololus]
 
-
 package game.core;
 
 import org.newdawn.slick.*;
@@ -12,44 +11,68 @@ public class Trololus extends BasicGame {
 	public Image background;
 	static int fpslimit = 60;
 	static String title = "Trololus NightBuild.0";
-	String menu1,menu2,menu3;
+	String menu1, menu2, menu3;
+	String S_ingame, S_loading, S_title, S_postgame;
 	static int width = 1000;
 	static int height = 500;
-	
-	
-	public Trololus(String title)	{super(title);
+	GameState state;
+
+	public Trololus(String title) {
+		super(title);
 	}
 
-	
 	@Override
-	public void init(GameContainer gc) throws SlickException{
+	public void init(GameContainer gc) throws SlickException {
 		background = new Image("resources/UI/menu.png");
-		menu1 = new String("rofl lol");
-		
-	} 
-	
+		state = GameState.INIT;
+
+
+	//initialize strings 
+		menu1 = "MENU1";
+		S_ingame = "INGAME";
+		S_loading = "The Game is loading! woooo";
+		S_title = "TROLOLUS NIGHTLY BUILD 0,0";
+		S_postgame = "The after game lobby.";
+	}
+
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
-		
+
 	}
-	
+
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException {
 		background.draw(0,0, (float)0.5);
 		
-		g.drawString(menu1, 100, 100);
-	}
+		switch (state) {
 	
+		case INIT:
+			g.drawString(menu1, 100, 100);
+			break;
+		case LOADING:
+			g.drawString(S_loading, 100, 100);
+			break;
+		case INGAME:
+			g.drawString(S_ingame, 100, 100);
+			break;
+		case POSTGAME:
+			g.drawString(S_postgame, 100, 100);
+			break;
+		default:
+			
+		
+	}}
+
 	public static void main(String[] args) throws SlickException {
 		System.out.println("pepa");
 		Pepta pepta = new Pepta();
 		pepta.Pozdrav();
-		
+
 		AppGameContainer app = new AppGameContainer(new Trololus(title));
 		app.setDisplayMode(width, height, fullscreen);
 		app.setSmoothDeltas(true);
 		app.setTargetFrameRate(fpslimit);
 		app.start();
-		
-}}
 
+	}
+}
--- src/game/core/map/MapType.java
+++ src/game/core/map/MapType.java
@@ -1,5 +1,5 @@
 package game.core.map;
 
 public enum MapType {
-
+Starlands;
 }
