ad3816bfa3e225490c468fa6d4ac391f466ebec4
David Kamberský <david.kambersky@gmail.com>
2013-01-03 22:25:14 +0100

    patching Pepta class [HYNKU SEE HERE]
    hynku, reviewni to, hazelo to chyby tak jsem neco premazal, kdyztak to
    revertni jeslti to bude nefungovat, diky :D
    D

--- src/game/core/Pepta.java
+++ src/game/core/Pepta.java
@@ -1,48 +1,11 @@
-<<<<<<< HEAD
+
 /*
  * PlayerList is the soon-to-be class responsible for creating players!!!
  * Pepta could be the program for sending the info to PlayerList
  * PEPTA = Player Existence Preparation Transcription Apparatus
+ * xC: trochu jsem to prekopal hazelo to errory koukni se jestli to funguje hynku
  */
-
-package game.core;
-
-import java.util.Scanner;
-
-public class Pepta {
-	static int players = 6;
-	
-	public void createPlayers(){
-		
-		Scanner wantedPlayers = new Scanner(System.in);
-		
-		System.out.println("Enter number of players");
-		players = (wantedPlayers.nextInt());
-		wantedPlayers.close();
-		
-		PlayerList playerlist = new PlayerList();
-		Player p = new Player();
-		for (int createdPlayers = 0; createdPlayers <= players ; createdPlayers++){
-			playerlist.createPlayer(p);
-		}
-		
-		System.out.println("Now you will be asked to enter players' names");
-		
-		Scanner nameScanner = new Scanner(System.in);
-		
-		
-		for (int namedPlayers = 1; namedPlayers < players; namedPlayers++){
-			System.out.println("Enter next player's name:");
-			playerlist.namePlayer(namedPlayers, nameScanner.nextLine());
-		}
-	}
-}
-=======
-/*
- * PlayerList is the soon-to-be class responsible for creating players!!!
- * Pepta could be the program for sending the info to PlayerList
- * PEPTA = Player Existence Preparation Transcription Apparatus
- */
+// 
 
 package game.core;
 
@@ -73,4 +36,3 @@ public class Pepta {
 		}
 	}
 }
->>>>>>> f4ed35bb1d3626a3c7bed78d09dc30d0bfbf3c3b
