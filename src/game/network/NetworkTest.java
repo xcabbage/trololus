e4d4c90806373511ce7e6ab88838fec67de189f5
DusanP <dusan.pilka@gmail.com>
2013-01-12 05:28:43 +0100

    NetworkTest class
    Quite extensive for debugging purposes. Needs threads and listening and
    stuff.

---
+++ src/game/network/NetworkTest.java
@@ -0,0 +1,34 @@
+package game.network;
+import java.io.*;
+import java.net.*;
+public class NetworkTest 
+{
+	PrintStream outStream = null;
+	DataInputStream inStream = null;
+	Socket testSocket = null;
+	public void connect(String server, int port)
+	{
+		try 
+			{
+			testSocket = new Socket(server, port);
+			System.out.println("Just connected to "+ testSocket.getRemoteSocketAddress());
+			outStream = new PrintStream(testSocket.getOutputStream()) ;
+			inStream = new DataInputStream(testSocket.getInputStream());
+			System.out.println("Streams connected");
+			}
+		catch (UnknownHostException e)
+			{
+			System.err.println("Don't know about host: " + server);
+			} 
+		catch (IOException e) 
+			{
+			System.err.println("Some kind of IO error occured. That stinks, doesnt it?");
+			}
+	}
+	public void sendMsg(String msg)
+	{
+		outStream.println(msg);
+		System.out.println("Sent the message "+msg+" to server");
+	}
+
+}
