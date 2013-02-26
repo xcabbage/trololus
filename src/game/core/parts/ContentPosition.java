fc01a008c0f72bc7cb84ec1029c478323fc24b04
David Kamberský <david.kambersky@gmail.com>
2013-02-26 18:57:18 +0100

    Fixing a bug with Git tracking for some reason, game/core/parts is ignored :O

---
+++ src/game/core/parts/ContentPosition.java
@@ -0,0 +1,14 @@
+package game.core.parts;
+
+/**
+ * The ContentPosition.java class responsible for
+ * 
+ * @author xCabbage [github.com/xcabbage]
+ * 
+ * @info for the Trololus project [github.com/xcabbage/trololus] created
+ *       26.2.2013 10:09:21
+ */
+public enum ContentPosition {
+	TopLeft, TopCenter, TopRight, CenterLeft, Center, CenterRight, BottomLeft, BottomCenter, BottomRight;
+
+}
