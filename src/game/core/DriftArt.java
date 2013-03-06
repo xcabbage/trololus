a415e0dae28487e11708d0edca497368265c77b5
xargo <xargo@seznam.cz>
2013-03-06 19:16:55 +0100

    DriftArt
    for smooth Hisoutensoku animations

---
+++ src/game/core/DriftArt.java
@@ -0,0 +1,31 @@
+package game.core;
+/**
+ * For moving objects that slow down as they aproach their location
+ * 
+ * @author Xargo
+ * 
+ * @info for the Trololus project [github.com/xcabbage/trololus] created
+ *       6.3.2013 18:33
+ */
+
+public class DriftArt {
+	int targetX;
+	int targetY;
+	int posX;
+	int posY;
+	int speed;
+	{
+		while ( (targetX-posX) * (targetX-posX) + (targetY - posY) * (targetY - posY) > 0 ) {
+			posX = posX + ((targetX-posX) / speed);
+			posY = posY + ((targetY-posY) / speed);
+			
+				if ( (targetX-posX) * (targetX-posX) + (targetY - posY) * (targetY - posY) < 1.2 ) {
+					posX = targetX;
+					posY = targetY;
+				}
+	
+		}
+	
+}	
+	
+}
