c61f23c91d64cd3ff023d1f112cbdafb45732c2b
David Kamberský <david.kambersky@gmail.com>
2014-11-25 20:55:06 +0100

    Moving Ship debug to a separate state

--- src/game/core/states/FifthState.java
+++ src/game/core/states/FifthState.java
@@ -18,22 +18,13 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class FifthState extends BasicState {
 
-	private static final int PROJECTILE_SPEED = 8;
+	public static final int ID = 4;
 
-	public static int ID = 4;
-
-	GameInstance instance;
-	Player player = new Player("Davefinek", Color.white);
-	boolean gameRunning = false;
 	FifthState state = this;
-	BattleField field;
-	int SPEED = 5;
-	Ship controlledShip;
-	Ship secondShip;
 
 	@Override
 	public void createContent() throws SlickException {
-		/* initTestingShip(); */
+
 		sb.addLabel(1, 0.5f, 0.5f, "News & Friends");
 		sb.getLabel(-1).setFont(Util.fontGreatHeader);
 		sb.getLabel(-1).setPosition(ContentPosition.TopCenter, 0, 50);
@@ -59,140 +50,11 @@ public class FifthState extends BasicState {
 		return ID;
 	}
 
-	void initTestingShip() throws SlickException {
-		instance = new GameInstance((BasicState) state, game, player);
-		field = instance.getField();
-		controlledShip = new Ship(ShipType.Striker, field);
-		secondShip = new Ship(ShipType.Striker, field);
-		field.placeEntity(controlledShip);
-		field.placeEntity(secondShip);
-		instance.centerShip(secondShip.getID());
-		instance.centerShip(controlledShip.getID());
-		gameRunning = true;
-	}
-
 	@Override
 	public void update(GameContainer gc, StateBasedGame mainGame, int delta)
 			throws SlickException {
 		super.update(gc, mainGame, delta);
-		// ship movement
 
-		if (gameRunning) {
-			field.updateProjectiles();
-			// rotation
-			if (input.isKeyDown(Input.KEY_A)) {
-				int reqRot = 0;
-				if (field.getRotation(controlledShip.getID()) - SPEED < 0) {
-					reqRot = field.getRotation(controlledShip.getID()) - SPEED
-							+ 360;
-
-				} else {
-					reqRot = field.getRotation(controlledShip.getID()) - SPEED;
-				}
-
-				field.setRotation(reqRot, controlledShip.getID());
-
-			}
-
-			if (input.isKeyDown(Input.KEY_D)) {
-				int reqRot = 0;
-				if (field.getRotation(controlledShip.getID()) + SPEED > 360) {
-					reqRot = field.getRotation(controlledShip.getID()) + SPEED
-							- 360;
-
-				} else {
-					reqRot = field.getRotation(controlledShip.getID()) + SPEED;
-				}
-				field.setRotation(reqRot, controlledShip.getID());
-			}
-
-			// movement
-			if (input.isKeyDown(Input.KEY_W)) {
-				field.moveShipForwards(controlledShip.getID());
-			}
-			if (input.isKeyDown(Input.KEY_S)) {
-				field.moveShipBackwards(controlledShip.getID());
-			}
-
-			// reset ship coords
-			if (input.isKeyPressed(Input.KEY_LCONTROL)) {
-				field.setShipX(500, controlledShip.getID());
-				field.setShipY(300, controlledShip.getID());
-				field.setRotation(0, controlledShip.getID());
-			}
-			if (input.isKeyPressed(Input.KEY_SPACE)) {
-				field.placeProjectile(field.getShipX(controlledShip.getID()),
-						field.getShipY(controlledShip.getID()) - 5,
-						field.getRotation(controlledShip.getID()),
-						PROJECTILE_SPEED, controlledShip.getID());
-
-				// field.placeProjectile(controlledShip.getCannonX(0),
-				// controlledShip.getCannonY(0),
-				// field.getRotation(controlledShip.getID()),
-				// PROJECTILE_SPEED, secondShip);
-			}
-			if (input.isKeyDown(Input.KEY_G)) {
-				field.placeProjectile(field.getShipX(controlledShip.getID()),
-						field.getShipY(controlledShip.getID()) + 5,
-						field.getRotation(controlledShip.getID()),
-						PROJECTILE_SPEED, controlledShip.getID());
-			}
-
-			if (input.isKeyPressed(Input.KEY_RCONTROL)) {
-
-				field.placeProjectile(field.getShipX(secondShip.getID()),
-						field.getShipY(secondShip.getID()) - 5,
-						field.getRotation(secondShip.getID()),
-						PROJECTILE_SPEED, secondShip.getID());
-
-				// field.placeProjectile(secondShip.getCannonX(0),
-				// secondShip.getCannonY(0),
-				// field.getRotation(secondShip.getID()),
-				// PROJECTILE_SPEED);
-			}
-
-			if (input.isKeyDown(Input.KEY_LEFT)) {
-				int reqRot = 0;
-				if (field.getRotation(secondShip.getID()) - SPEED < 0) {
-					reqRot = field.getRotation(secondShip.getID()) - SPEED
-							+ 360;
-
-				} else {
-					reqRot = field.getRotation(secondShip.getID()) - SPEED;
-				}
-
-				field.setRotation(reqRot, secondShip.getID());
-
-			}
-
-			if (input.isKeyDown(Input.KEY_RIGHT)) {
-				int reqRot = 0;
-				if (field.getRotation(secondShip.getID()) + SPEED > 360) {
-					reqRot = field.getRotation(secondShip.getID()) + SPEED
-							- 360;
-
-				} else {
-					reqRot = field.getRotation(secondShip.getID()) + SPEED;
-				}
-				field.setRotation(reqRot, secondShip.getID());
-			}
-
-			// movement
-			if (input.isKeyDown(Input.KEY_UP)) {
-				field.moveShipForwards(secondShip.getID());
-			}
-			if (input.isKeyDown(Input.KEY_DOWN)) {
-				field.moveShipBackwards(secondShip.getID());
-			}
-
-			if (input.isKeyPressed(Input.KEY_P)) {
-				field.mapBoundsCheckStop();
-			}
-			if (input.isKeyPressed(Input.KEY_T)) {
-				field.mapBoundsCheckStart(2500);
-			}
-
-		}
 	}
 
 	@Override
@@ -204,9 +66,9 @@ public class FifthState extends BasicState {
 
 			e.printStackTrace();
 		}
-		instance.draw(g);
-	}
 
+	}
+	
 	@Override
 	public void render(GameContainer gc, StateBasedGame mainGame, Graphics g)
 			throws SlickException {
--- src/game/nonstatic/system/Pepta.java
+++ src/game/nonstatic/system/Pepta.java
@@ -1,5 +1,4 @@
-/*
- * PlayerList is the soon-to-be class responsible for creating players!!!
+/* * PlayerList is the soon-to-be class responsible for creating players!!!
  * Pepta could be the program for sending the info to PlayerList
  * PEPTA = Player Existence Preparation Transcription Apparatus
  * xC: trochu jsem to prekopal hazelo to errory koukni se jestli to funguje hynku
@@ -7,30 +6,27 @@
 
 package game.nonstatic.system;
 
-
 import java.util.Scanner;
-
 
 public class Pepta {
 	static int players = 0;
-	
-	public void createPlayers(){
+
+	public void createPlayers() {
 		/*
-		Scanner wantedPlayers = new Scanner(System.in);
-		
-		System.out.println("enter number of players");
-		players = (wantedPlayers.nextInt());
-		wantedPlayers.close();
-		*/
+		 * Scanner wantedPlayers = new Scanner(System.in);
+		 * 
+		 * System.out.println("enter number of players"); players =
+		 * (wantedPlayers.nextInt()); wantedPlayers.close();
+		 */
 		PlayerList playerlist = new PlayerList();
 		Player p = new Player();
-		for (int x = 0; x <= players ; x++){
+		for (int x = 0; x <= players; x++) {
 			playerlist.createPlayer(p);
 		}
-		
+
 		Scanner nameScanner = new Scanner(System.in);
-		
-		for (int createdPlayers = 1; createdPlayers < players; createdPlayers++){
+
+		for (int createdPlayers = 1; createdPlayers < players; createdPlayers++) {
 			System.out.println("Enter next player's name:");
 			playerlist.namePlayer(createdPlayers, nameScanner.nextLine());
 		}
