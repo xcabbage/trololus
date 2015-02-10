62de60daba85bc2248ea72cc008bdb1c1f7e55ed
David Kamberský <david.kambersky@gmail.com>
2015-02-10 11:32:25 +0100

    HUD added

---
+++ src/game/core/states/gamestate/HUD.java
@@ -0,0 +1,19 @@
+package game.core.states.gamestate;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.GameState;
+
+import game.core.parts.Component;
+
+public class HUD extends Component {
+
+	public HUD(GameState state, GameContainer gc, Graphics g) {
+		super(state, gc, g);
+		
+	}
+
+	public void init(){
+		
+	}
+}
